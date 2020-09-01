6e01cf5b14cef0ae0ceb6df21ca089d11fb1836c
James Henager <jameschenager@gmail.com>
2020-09-01 15:16:35 -0700

    add names to plants
    Co-authored-by: Deryck Jackson <Dgjackson01@gmail.com>

--- src/main.js
+++ src/main.js
@@ -1,7 +1,18 @@
-// This function stores our state.
+// This function stores our state. No initial values.
 
-const storeState = () => {
-  let currentState = {};
+// const storeState = () => {
+//   let currentState = {};
+//   return (stateChangeFunction = state => state) => {
+//     const newState = stateChangeFunction(currentState);
+//     currentState = {...newState};
+//     return newState;
+//   }
+// }
+
+// Function factory that accounts for initial state values
+
+const storeState = (initialState) => {
+  let currentState = initialState;
   return (stateChangeFunction = state => state) => {
     const newState = stateChangeFunction(currentState);
     currentState = {...newState};
@@ -9,8 +20,11 @@ const storeState = () => {
   }
 }
 
-const plant = storeState();
-const plant2 = storeState();
+// Instantiated objects are equal to the store state. Initial values are passed in.
+
+const initialValues = { name: " ", soil: 5, water: 5 };
+const plant = storeState(initialValues);
+const plant2 = storeState(initialValues);
 
 // This is a function factory. We can easily create more specific functions that alter a plant's soil, water, and light to varying degrees.
 
@@ -24,24 +38,37 @@ const changeState = (prop) => {
 }
 
 // We create four functions using our function factory. We could easily create many more.
-// const plant1 = { "soil": 0, "water": 0};
-// const plant2 = { "soil": 0, "water": 0};
+
 
 const feed = changeState("soil")(1);
 const blueFood = changeState("soil")(5);
 
 const hydrate = changeState("water")(1);
 const superWater = changeState("water")(5);
+const bob = changeState("name")("Bob");
+const terrance = changeState("name")("Terrance");
+
 
 $(document).ready(function() {
-  // const plantState = storeState({"name": "Jerry", "soil": 0});
-  // const plantName = plantState("name")();
-  // $('#name-value').text(`Name: ${plantName.name}`);
+  const plantOneState = plant(bob);
+  $('#name-value1').text(`${plantOneState.name}`);
+  $('#soil-value1').text(`Soil: ${plantOneState.soil}`);
+  $('#water-value1').text(`Water: ${plantOneState.water}`);
+
+  const plantTwoState = plant2(terrance);
+  $('#name-value2').text(`${plantTwoState.name}`);
+  $('#soil-value2').text(`Soil: ${plantTwoState.soil}`);
+  $('#water-value2').text(`Water: ${plantTwoState.water}`);
   
-// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. Note that we only use one of our functions to alter soil. You can easily add more.
-  $('#water').click(function() {
+// This function has side effects because we are using jQuery. Manipulating the DOM will always be a side effect. 
+  $('#water1').click(function() {
     const newState = plant(hydrate);
-    $('#water-value').text(`Water: ${newState.water}`);
+    $('#water-value1').text(`Water: ${newState.water}`);
+  });
+
+  $('#water2').click(function() {
+    const newState = plant2(hydrate);
+    $('#water-value2').text(`Water: ${newState.water}`);
   });
   
 
@@ -55,12 +82,18 @@ $(document).ready(function() {
     $('#soil-value2').text(`Soil: ${newState2.soil}`);
   });
 
-// This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
+// Currently redundant as the values are displayed on the page by default * This function doesn't actually do anything useful in this application - it just demonstrates how we can "look" at the current state (which the DOM is holding anyway). However, students often do need the ability to see the current state without changing it so it's included here for reference.
 
-  $('#show-state').click(function() {
+  $('#show-state1').click(function() {
     // We just need to call stateControl() without arguments to see our current state.
     const currentState = plant();
-    $('#soil-value').text(`Soil: ${currentState.soil}`);
-    $('#water-value').text(`Water: ${currentState.soil}`);
+    $('#soil-value1').text(`Soil: ${currentState.soil}`);
+    $('#water-value1').text(`Water: ${currentState.water}`);
+  });
+
+  $('#show-state2').click(function() {
+    const currentState = plant2();
+    $('#soil-value2').text(`Soil: ${currentState.soil}`);
+    $('#water-value2').text(`Water: ${currentState.water}`);
   });
 });
